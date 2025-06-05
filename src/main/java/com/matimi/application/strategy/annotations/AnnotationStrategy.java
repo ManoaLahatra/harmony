339af8018d722a34a7560b54d63353171c21c7a3
ManoaLahatra <manoalahatra07@gmail.com>
2025-06-05 10:14:25 +0300

    chore: add some consistency to AnnotationStrategy

--- src/main/java/com/matimi/application/strategy/annotations/AnnotationStrategy.java
+++ src/main/java/com/matimi/application/strategy/annotations/AnnotationStrategy.java
@@ -1,8 +1,13 @@
 package com.matimi.application.strategy.annotations;
 
+import java.util.List;
 import java.util.Scanner;
 
 public interface AnnotationStrategy {
+    boolean isApplicable(String fieldType);
     boolean shouldApply(Scanner scanner);
     String generateAnnotation();
+    default List<String> getRequiredImports() {
+        return List.of();
+    }
 }
